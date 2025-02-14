4e81d1ac71c5ee98c172ba927d23a8bed83b3e21
Wordbricks <123422550+wordbricks-dev@users.noreply.github.com>
2025-02-14 15:45:16 +0900

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import RainingLetters from "../components/RainingLetters"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <RainingLetters />
+    </main>
+  )
+}
+
