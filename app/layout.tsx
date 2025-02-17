b9fe21e1b9af9f27ba2588a9b3e223da074a9f0b
Wordbricks <123422550+wordbricks-dev@users.noreply.github.com>
2025-02-17 19:47:17 +0900

    Update layout.tsx
    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+export { metadata } from './metadata'
 
 export default function RootLayout({
   children,
